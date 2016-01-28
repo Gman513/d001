a2c3d30798d4aed04339a519fc2f59b77ca4660d
12345 <a12345@pampadump.42.fr>
2016-01-28 13:00:32 +0200

    words

---
+++ ex04/MAC.sh
@@ -0,0 +1,7 @@
+	ether 38:c9:86:0c:8d:a3 
+	ether 28:f0:76:07:3b:80 
+	ether 1a:00:00:2a:24:70 
+	ether 1a:00:00:2a:24:71 
+	ether 0a:f0:76:07:3b:80 
+	ether 3a:c9:86:c0:7b:00 
+	ether 1a:8a:26:94:09:3b 
---
+++ ex09/add_chelou.sh
@@ -0,0 +1,5 @@
+export FT_NBR1=\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"\!\\\"\\\"
+
+export FT_NBR2=dcrcmcmooododmrrrmorcmcrmomo
+
+echo $FT_NBR1 + $FT_NBR2…??????
